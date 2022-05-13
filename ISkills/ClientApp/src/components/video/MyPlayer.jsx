f6753a31214f9bec8062f3022727cb0d2189dd1b
Raimhal <kormish2003@gmail.com>
2022-05-13 12:10:45 +0300

    .

--- ISkills/ClientApp/src/components/video/MyPlayer.jsx
+++ ISkills/ClientApp/src/components/video/MyPlayer.jsx
@@ -1,25 +1,21 @@
 import React, {useEffect} from 'react';
 import 'mui-player/dist/mui-player.min.css'
 import MuiPlayer from 'mui-player'
+import ReactPlayer from 'react-player'
 
 
 const MyPlayer = ({video, ...props}) => {
-    let player;
-    useEffect(() => {
-        player = new MuiPlayer({
-            container: `#v${video.id}`,
-            src: video.url,
-            title: video.title,
-            preload: "auto",
-            showMiniProgress: true,
-            fitContent: true,
-            controls: true,
-        })
-    })
     return (
-        <div id={`v${video.id}`} {...props} >
-
-        </div>
+        <ReactPlayer
+            url={video.url}
+            controls={true}
+            controlsList="nodownload"
+            playsinline={true}
+            pip={true}
+            width="60vw"
+            height="auto"
+            poster={video.title}
+        />
     );
 };
 
