0917ee48c8a537045bd40b6f4fbe10b65c53901a
Raimhal <kormish2003@gmail.com>
2022-05-29 12:13:45 +0300

    test deploy 4

--- ISkills/ClientApp/src/App.js
+++ ISkills/ClientApp/src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import {useDispatch} from "react-redux";
 import {getCurrentUser} from "./store/UserReducer";
-import Navbar from "components/UI/Navbar/Navbar";
+// import Navbar from "./components/UI/Navbar/Navbar";
 
 const App = () => {
     const dispatch = useDispatch()
@@ -20,7 +20,7 @@ const App = () => {
    return (
        <div className="App">
            <BrowserRouter>
-               <Navbar/>
+               {/*<Navbar/>*/}
                <AppRouter/>
            </BrowserRouter>
        </div>
