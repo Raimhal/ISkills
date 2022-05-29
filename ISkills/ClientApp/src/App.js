43ff8347a103839fcb02806679faca1f7571b32b
Raimhal <kormish2003@gmail.com>
2022-05-29 11:38:22 +0300

    test deploy 2

--- ISkills/ClientApp/src/App.js
+++ ISkills/ClientApp/src/App.js
@@ -2,10 +2,10 @@ import React, {useEffect} from 'react'
 import "./styles/App.css"
 import "./styles/Course.css"
 import {BrowserRouter} from "react-router-dom";
-import Navbar from "./components/UI/Navbar/Navbar";
 import AppRouter from "./components/AppRouter";
-import {useDispatch, useSelector} from "react-redux";
-import {getCurrentUser, setIsAdmin, setIsAuth, setUser} from "./store/UserReducer";
+import {useDispatch} from "react-redux";
+import {getCurrentUser} from "./store/UserReducer";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 const App = () => {
     const dispatch = useDispatch()
