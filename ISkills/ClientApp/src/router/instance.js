e507123e37293d1d9701f37c2feb898bfecbed78
Raimhal <kormish2003@gmail.com>
2022-06-01 11:44:33 +0300

    .

--- ISkills/ClientApp/src/router/instance.js
+++ ISkills/ClientApp/src/router/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-    baseURL: `./api`,
+    baseURL: `https://iskills-app.herokuapp.com/api`,
 });
 
 instance.interceptors.request.use((config) => {
